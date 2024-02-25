c995c0e7214b49c129981f23148a3af3d6e5b9c6
vezorgoat <73618894+vezorgoat@users.noreply.github.com>
2024-02-25 09:38:33 -0600

    Update lcd2.py

--- lcd2.py
+++ lcd2.py
@@ -8,8 +8,8 @@ lcd_columns = 16
 lcd_rows = 2
 
 # Define GPIO pins for SCL and SDA
-scl_pin = board.D4  # Pin 7 (GPIO 4) for SCL
-sda_pin = board.D17  # Pin 11 (GPIO 17) for SDA
+scl_pin = board.D27  # Pin 27 (GPIO 0) for SCL
+sda_pin = board.D28  # Pin 28 (GPIO 1) for SDA
 
 # Initialize I2C bus with specified pins
 i2c = busio.I2C(scl_pin, sda_pin)
@@ -21,7 +21,7 @@ lcd = character_lcd.Character_LCD_I2C(i2c, lcd_columns, lcd_rows)
 lcd.backlight = True
 
 # Display message
-lcd.message = "Hello, World!"
+lcd.message = "Hello, World!\nWelcome to\nRaspberry Pi!"
 
 # Wait for 5 seconds
 time.sleep(5)
