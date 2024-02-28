371ddf835aa844355e48c6746929767951d7cb64
vezorgoat <73618894+vezorgoat@users.noreply.github.com>
2024-02-27 20:37:43 -0600

    Update Tdslcd.py

--- Tdslcd.py
+++ Tdslcd.py
@@ -1,14 +1,10 @@
-import os
-import glob
 import time
 import smbus
+import sys
+from CQRobot_ADS1115 import ADS1115
 import drivers
-from time import sleep
 from datetime import datetime
-
-# Mount the temperature sensor device
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
+from time import sleep
 
 # Define I2C address and register addresses for the TDS sensor
 TDS_ADDRESS = 0x48  # I2C address of the TDS sensor
@@ -17,32 +13,14 @@ TDS_CMD_READ = 0x00  # Command to read data from the sensor
 # Create an instance of the smbus for I2C communication
 bus = smbus.SMBus(1)  # Use bus 1 for Raspberry Pi 3, use 0 for older models
 
-# Set up the path to the temperature sensor
-base_dir = '/sys/bus/w1/devices/'
-device_path = glob.glob(base_dir + '28*')[0]  # Get file path of sensor
+# Initialize ADS1115 ADC for pH sensor
+ads1115 = ADS1115()
+ads1115.setAddr_ADS1115(0x48)  # Set the I2C address of the ADC
+ads1115.setGain(ADS1115_REG_CONFIG_PGA_6_144V)  # Set the gain to 6.144V
+calibration_value = 7.0  # Calibration value obtained from your calibration process
 
-# Initialize the LCD display
+# Load the driver and set it to "display"
 display = drivers.Lcd()
-
-def read_temp_raw():
-    with open(device_path + '/w1_slave', 'r') as f:
-        valid, temp = f.readlines()
-    return valid, temp
-
-def read_temp():
-    valid, temp = read_temp_raw()
-
-    while 'YES' not in valid:
-        time.sleep(0.2)
-        valid, temp = read_temp_raw()
-
-    pos = temp.index('t=')
-    if pos != -1:
-        # Read the temperature.
-        temp_string = temp[pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * (9.0 / 5.0) + 32.0
-        return temp_c, temp_f
 
 def read_tds_value():
     # Send command to request TDS data
@@ -55,39 +33,45 @@ def read_tds_value():
     tds_value = (data[0] << 8) | data[1]
     return tds_value
 
+def read_ph_value():
+    # Read analog voltage from pH sensor
+    analog_voltage = ads1115.readVoltage(0)['r']  # Assuming pH sensor is connected to channel 0
+
+    # Calculate pH value using the calibration value
+    ph_value = 14.0 - (analog_voltage + calibration_value) / 1000.0  # Adjust this formula as needed
+    return ph_value
+
 try:
     while True:
         # Clear the display
         display.lcd_clear()
 
-        # Get current temperature and determine status
-        temp_c, temp_f = read_temp()
-        if 0 <= temp_c <= 30:
-            temp_status = "Mid"
-        elif 30 < temp_c <= 70:
-            temp_status = "Good"
-        elif 80 <= temp_c <= 120:
-            temp_status = "Bad"
-        else:
-            temp_status = "Unknown"
-
-        # Read TDS value from the sensor
-        tds = read_tds_value()
-
         # Get current time
         current_time = datetime.now().strftime("%I:%M %p")
+
+        # Get TDS value
+        tds_value = read_tds_value()
+
+        # Get pH value
+        ph_value = read_ph_value()
+
+        # Determine TDS status
+        if 0 <= tds_value <= 30:
+            tds_status = "Mid"
+        elif 30 < tds_value <= 70:
+            tds_status = "Good"
+        elif 80 <= tds_value <= 120:
+            tds_status = "Bad"
+        else:
+            tds_status = "Unknown"
 
         # Write time to display
         display.lcd_display_string("Time: " + current_time, 1)
 
-        # Write temperature and status to display
-        display.lcd_display_string("Temp: {:.1f}C {:.1f}F".format(temp_c, temp_f), 2)
-        display.lcd_display_string("Status: " + temp_status, 3)
+        # Write TDS and pH values to display
+        display.lcd_display_string("TDS: {}   pH: {:.2f}".format(tds_status, ph_value), 2)
 
-        # Write TDS value to display
-        display.lcd_display_string("TDS: {}".format(tds), 4)
-
-        sleep(2)
+        sleep(1)
 
 except KeyboardInterrupt:
     # If there is a KeyboardInterrupt, exit the program and cleanup
